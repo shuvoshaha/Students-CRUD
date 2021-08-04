ca17f11c68493054fbfc47c85aac41780f07c519
SKS <shaha460@gmail.com>
2021-08-04 11:33:52 +0600

    get and set student in controller

--- server/controller/students.js
+++ server/controller/students.js
@@ -1,3 +1,20 @@
-export const postStudents = async (req, res) =>{
-    res.send("Route is working...")
+
+export const postStudents = async (req, res) => {
+
+    const getStudentInfo = req.body;
+    const newStudent = new student(getStudentInfo);
+
+    try {
+        await newStudent.save();
+    }
+    catch (err) { res.status(404).json({ message: err.message }) }
+}
+
+
+export const getStudentsData = async (req, res) => {
+    try {
+        const allStudents = student.find();
+        res.status(200).json(allStudents);
+    }
+    catch (err) { res.status(404).json({ message: err.message }) }
 }
--- server/route/students.js
+++ server/route/students.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { postStudents } from '../controller/students.js'
+import student from '../model/students.js'
 
 const router = express.Router()
 
-router.get('/', postStudents)
+router.get('/', )
+router.post('/', postStudents)
 
 export default router
 
